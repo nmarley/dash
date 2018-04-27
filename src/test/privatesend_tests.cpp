d1dd57ceac68ca4d26540be7174b98b44a1ab668
Nathan Marley <nathan.marley@gmail.com>
2018-04-26 21:47:05 -0700

    fix

--- src/test/privatesend_tests.cpp
+++ src/test/privatesend_tests.cpp
@@ -12,25 +12,13 @@ BOOST_FIXTURE_TEST_SUITE(privatesend_tests, BasicTestingSetup)
 /* Test STUFF */
 BOOST_AUTO_TEST_CASE(privatesend_test)
 {
+    CPrivateSend::InitStandardDenominations();
+
     int nCount;
-    nCount = privatesend.getCount();
+    nCount = CPrivateSend::vecStandardDenominations.size();
 
     // privatesend.getEntries()
-    BOOST_CHECK_EQUAL(nCount, 0);
-
-    privatesend.add("mn1", "127.0.0.1:9999", "cT6dc7T42UrnDWbU1MAVr7fc6nCinH76FtY3GZVQJTiCpXuf2t5v", "a2be5bc8caefcde822cecfff34dad384aef165d8ae163a062dea43323ca63ac9", "1");
-    privatesend.add("mn2", "127.0.0.2:9999", "cT6dc7T42UrnDWbU1MAVr7fc6nCinH76FtY3GZVQJTiCpXuf2t5x", "b2be5bc8caefcde822cecfff34dad384aef165d8ae163a062dea43323ca63ac9", "0");
-    // should break (in theory)
-    privatesend.add("mn2", "127.0.0.3:9999", "xT6dc7T42UrnDWbU1MAVr7fc6nCinH76FtY3GZVQJTiCpXuf2t5x", "b2be5bc8caefcde822cecfff34dad384aef165d8ae163a062dea43323ca63ac9", "0");
-
-    nCount = privatesend.getCount();
-    BOOST_CHECK_EQUAL(nCount, 2);
-
-    const auto& mne = privatesend.findByAlias("mn1");
-    BOOST_CHECK_EQUAL(mne.getPrivKey(), "cT6dc7T42UrnDWbU1MAVr7fc6nCinH76FtY3GZVQJTiCpXuf2t5v");
-
-    const auto& mne2 = privatesend.findByAlias("mn2");
-    BOOST_CHECK_EQUAL(mne2.getPrivKey(), "xT6dc7T42UrnDWbU1MAVr7fc6nCinH76FtY3GZVQJTiCpXuf2t5x");
+    BOOST_CHECK_EQUAL(nCount, 4);
 
     // break something so that I know tests are running
     BOOST_CHECK_EQUAL(0, 1);
