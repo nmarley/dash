903e502bb8d3a266da839ed81434d69e2565fbdc
Nathan Marley <nathan.marley@gmail.com>
2018-04-29 09:25:23 -0700

    more tests

--- src/test/privatesend_tests.cpp
+++ src/test/privatesend_tests.cpp
@@ -22,6 +22,15 @@ BOOST_AUTO_TEST_CASE(privatesend_test)
     // ensure 4 standard denominations defined
     BOOST_CHECK_EQUAL(nCount, 4);
 
+    // ensure smallest denomination is the expected value
+    CAmount nSmallestDenom = CPrivateSend::GetSmallestDenomination();
+    BOOST_CHECK_EQUAL(nSmallestDenom, 1000010); // (0.01 * COIN) + 10
+
+    // TODO: IMPLEMENT THIS METHOD
+    // ensure largest denomination is the expected value
+    // CAmount nLargestDenom = CPrivateSend::GetLargestDenomination();
+    // BOOST_CHECK_EQUAL(nLargestDenom, 1000010000); // (10 * COIN) + 10000
+
     // break something so that I know tests are running
     BOOST_CHECK_EQUAL(0, 1);
 }
