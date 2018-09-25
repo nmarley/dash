36f2183c5c32184c2b79463c6f21ab15538570eb
Nathan Marley <nathan.marley@gmail.com>
2018-09-24 22:05:34 -0700

    debug

--- src/test/mnp_tests.cpp
+++ src/test/mnp_tests.cpp
@@ -8,7 +8,7 @@
 #include "test/test_dash.h"
 #include <boost/test/unit_test.hpp>
 
-BOOST_FIXTURE_TEST_SUITE(mnp_tests, BasicTestingSetup)
+BOOST_FIXTURE_TEST_SUITE(mnp_tests, TestChain100Setup)
 
 // private key for this WIF / address:
 // 0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
@@ -17,9 +17,41 @@ std::string sporkAddr = "yMtMWAjPhUquwKtdG4wzj9Cpn4asQkLV8F";
 
 BOOST_AUTO_TEST_CASE(MNP_GetHash)
 {
-    // CMasternodePing
-    mnp = CMasternodePing();
+    // COutPoint
+    COutPoint x = COutPoint(uint256S("0x147caa76786596590baa4e98f5d9f48b86c7765e489f7a6ff3360fe5c674360b"), 0);
+    const COutPoint& ref = x;
+    CMasternodePing mnp = CMasternodePing(ref);
+
+    //     CHashWriter ss(SER_GETHASH, PROTOCOL_VERSION);
+    //     ss << masternodeOutpoint << uint8_t{} << 0xffffffff; // adding dummy values here to match old hashing format
+    //     ss << sigTime;
+    //     return ss.GetHash();
+
+    // debugging
+    //     ss << masternodeOutpoint << uint8_t{} << 0xffffffff; // adding dummy values here to match old hashing format
+    // std::cout << "NGM DEBUGGING: mnp.masternodeOutpoint: " << mnp.masternodeOutpoint << std::endl;
+
+    // class COutPoint {
+    // public:
+    //     uint256 hash;
+    //     uint32_t n;
+
+    std::cout << "NGM DEBUGGING: mnp.masternodeOutpoint.hash: " << mnp.masternodeOutpoint.hash.ToString() << std::endl;
+    std::cout << "NGM DEBUGGING: mnp.masternodeOutpoint.n: " << mnp.masternodeOutpoint.n << std::endl;
+
+    std::cout << "NGM DEBUGGING: mnp.sigTime: " << mnp.sigTime << std::endl;
+    std::cout << "NGM DEBUGGING: mnp.GetHash(): " << mnp.GetHash().ToString() << std::endl;
 }
+
+// CMasternodePing::CMasternodePing(const COutPoint& outpoint) {
+//    LOCK(cs_main);
+//    if (!chainActive.Tip() || chainActive.Height() < 12) return;
+//    masternodeOutpoint = outpoint;
+//    blockHash = chainActive[chainActive.Height() - 12]->GetBlockHash();
+//    sigTime = GetAdjustedTime();
+//    nDaemonVersion = CLIENT_VERSION;
+// }
+
 
 //BOOST_AUTO_TEST_CASE(SporkManager__SetPrivKey)
 //{
