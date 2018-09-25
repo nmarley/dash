580aa3e8fd4b8daaf0bddf0ca515430b8b08b3ee
Ubuntu <ubuntu@example.com>
2018-09-25 16:37:07 +0000

    remove more

--- src/test/crypto_tests.cpp
+++ src/test/crypto_tests.cpp
@@ -3,12 +3,6 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "crypto/aes.h"
-#include "crypto/ripemd160.h"
-#include "crypto/sha1.h"
-#include "crypto/sha256.h"
-#include "crypto/sha512.h"
-#include "crypto/hmac_sha256.h"
-#include "crypto/hmac_sha512.h"
 #include "utilstrencodings.h"
 #include "test/test_dash.h"
 #include "test/test_random.h"
@@ -21,35 +15,6 @@
 #include <openssl/evp.h>
 
 BOOST_FIXTURE_TEST_SUITE(crypto_tests, BasicTestingSetup)
-
-template<typename Hasher, typename In, typename Out>
-void TestVector(const Hasher &h, const In &in, const Out &out) {
-    Out hash;
-    BOOST_CHECK(out.size() == h.OUTPUT_SIZE);
-    hash.resize(out.size());
-    {
-        // Test that writing the whole input string at once works.
-        Hasher(h).Write((unsigned char*)&in[0], in.size()).Finalize(&hash[0]);
-        BOOST_CHECK(hash == out);
-    }
-    for (int i=0; i<32; i++) {
-        // Test that writing the string broken up in random pieces works.
-        Hasher hasher(h);
-        size_t pos = 0;
-        while (pos < in.size()) {
-            size_t len = insecure_rand() % ((in.size() - pos + 1) / 2 + 1);
-            hasher.Write((unsigned char*)&in[pos], len);
-            pos += len;
-            if (pos > 0 && pos + 2 * out.size() > in.size() && pos < in.size()) {
-                // Test that writing the rest at once to a copy of a hasher works.
-                Hasher(hasher).Write((unsigned char*)&in[pos], in.size() - pos).Finalize(&hash[0]);
-                BOOST_CHECK(hash == out);
-            }
-        }
-        hasher.Finalize(&hash[0]);
-        BOOST_CHECK(hash == out);
-    }
-}
 
 void TestAES256CBC(const std::string &hexkey, const std::string &hexiv, bool pad, const std::string &hexin, const std::string &hexout)
 {
@@ -75,8 +40,7 @@ void TestAES256CBC(const std::string &hexkey, const std::string &hexiv, bool pad
     BOOST_CHECK_MESSAGE(decrypted == in, HexStr(decrypted) + std::string(" != ") + hexin);
 
     // Encrypt and re-decrypt substrings of the plaintext and verify that they equal each-other
-    for(std::vector<unsigned char>::iterator i(in.begin()); i != in.end(); ++i)
-    {
+    for(std::vector<unsigned char>::iterator i(in.begin()); i != in.end(); ++i) {
         std::vector<unsigned char> sub(i, in.end());
         std::vector<unsigned char> subout(sub.size() + AES_BLOCKSIZE);
         int _size = enc.Encrypt(&sub[0], sub.size(), &subout[0]);
