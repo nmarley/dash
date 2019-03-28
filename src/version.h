bfb2fee9af685496158fd1be36e5bf252f7c20b0
Nathan Marley <nathan.marley@gmail.com>
2019-03-28 18:16:24 -0300

    Superblock Megasquash 5
    superblock work - megasquash
    
    clean up a bit
    
    Add new Trigger / Proposal formats
    
    test debug
    
    fix
    
    fix2
    
    univalue style
    
    pair it up
    
    Order payments, const GetHash
    
    reverse sort payments
    
    update
    
    digest debug
    
    Remove trailing whitespace
    
    Begin removal of CBitcoinAddress
    
    Use new SERIALIZE_METHODS
    
    Fix rebase issue w/header guard endif
    
    Fix LogPrint category
    
    s/Address/Dest in gov
    
    Add mn header file in gov
    
    Replace activeMNMgr
    
    Remove extra arg from IsValidLocally
    
    s/CBitcoinAddress/CTxDest/
    
    arith256
    
    Remove import from validators.h
    
    Remove payeeAddr (now is payoutDest)
    
    Fix Pair() crap
    
    Fix addr thing
    
    Fix renamed test headers
    
    Add missing gov header in gov/obj
    
    Comment code-smelly things
    
    Comment shit that is breaking the build
    
    Make payoutDest a pointer
    
    Use script
    
    Fix littles things
    
    Rm trailing spaces
    
    Remove commented

--- src/version.h
+++ src/version.h
@@ -56,4 +56,8 @@ static const int GOVSCRIPT_PROTO_VERSION = 70221;
 
 // Make sure that none of the values above collide with `ADDRV2_FORMAT`.
 
+//! Superblock triggers created by DashCore
+static const int CORE_SUPERBLOCKS_PROTO_VERSION = 70221;
+
+
 #endif // BITCOIN_VERSION_H
