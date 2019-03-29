d11c05d6a41e1f1282f3d89c93c264dcedb39cd2
Nathan Marley <nathan.marley@gmail.com>
2019-03-29 18:50:07 -0300

    more loggin

--- src/governance.cpp
+++ src/governance.cpp
@@ -669,6 +669,7 @@ bool CGovernanceManager::CreateSBTrigger() {
     CAmount nBudgetUsed(0);
     std::vector<const CGovernanceObject*> vFinalProposals;
     for (auto pGovObj : vProposals) {
+        LogPrint("gobject", "NGM analyzing proposal %s, votes: %d\n", pGovObj->GetHash(), pGovObj->GetAbsoluteYesCount(VOTE_SIGNAL_FUNDING));
         auto v = CProposalValidator(pGovObj->GetDataAsHexString(), false);
         auto deets = v.GetProposalDetail();
         LogPrint("gobject", "NGM got deets\n");
