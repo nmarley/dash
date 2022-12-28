99987b2b55ce583da65a85500618fae032db25c5
Odysseas Gabrielides <odysseas.gabrielides@gmail.com>
2022-12-28 14:41:52 +0200

    Added v20 HF

--- src/llmq/utils.h
+++ src/llmq/utils.h
@@ -95,6 +95,7 @@ Consensus::LLMQType GetInstantSendLLMQType(bool deterministic);
 bool IsDIP0024Active(const CBlockIndex* pindex);
 bool IsV19Active(const CBlockIndex* pindex);
 const CBlockIndex* V19ActivationIndex(const CBlockIndex* pindex);
+bool IsV20Active(const CBlockIndex* pindex);
 static bool IsInstantSendLLMQTypeShared();
 
 /// Returns the state of `-llmq-data-recovery`
