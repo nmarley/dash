4728ed31954f08b50195c6e693470f13fce30025
Nathan Marley <nathan@glowberrylabs.com>
2026-03-07 08:16:02 -0800

    Expand transaction API response data
    - Add Transaction type import
    - Import script analysis and encoding functions
    - Extend TxResponse struct with additional fields
    - Support script type analysis and address encoding

--- daino/crates/daino-serve/src/api.rs
+++ daino/crates/daino-serve/src/api.rs
@@ -20,8 +20,9 @@ use serde::Serialize;
 
 use daino_fetch::rpc::DashdRpc;
 use daino_state::db::DainoDB;
+use librustdash::Transaction;
 use librustdash::hash::{hash_to_display, reverse_hash};
-use librustdash::script::decode_address;
+use librustdash::script::{ScriptType, analyze_script, decode_address, encode_address};
 
 /// Shared application state passed to all handlers.
 pub struct AppState {
@@ -86,26 +87,85 @@ pub struct BlockHashResponse {
 #[derive(Serialize)]
 pub struct TxResponse {
     pub txid: String,
-    #[serde(rename = "blockheight")]
-    pub block_height: u32,
-    #[serde(rename = "txindex")]
-    pub tx_index: u32,
     pub version: i16,
-    #[serde(rename = "type")]
-    pub tx_type: u16,
+    #[serde(rename = "type", skip_serializing_if = "Option::is_none")]
+    pub tx_type: Option<u16>,
     pub locktime: u32,
+    pub vin: Vec<TxVin>,
+    pub vout: Vec<TxVout>,
+    pub blockhash: String,
+    #[serde(rename = "blockheight")]
+    pub block_height: i32,
+    pub confirmations: u32,
+    pub time: u32,
+    pub blocktime: u32,
+    #[serde(rename = "isCoinBase", skip_serializing_if = "Option::is_none")]
+    pub is_coinbase: Option<bool>,
     #[serde(rename = "valueOut")]
     pub value_out: f64,
-    #[serde(rename = "inputCount")]
-    pub input_count: u32,
-    #[serde(rename = "outputCount")]
-    pub output_count: u32,
-    /// InstantSend lock status (None if dashd not connected)
+    pub size: usize,
+    #[serde(rename = "valueIn", skip_serializing_if = "Option::is_none")]
+    pub value_in: Option<f64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub fees: Option<f64>,
     #[serde(rename = "txlock", skip_serializing_if = "Option::is_none")]
     pub txlock: Option<bool>,
-    /// ChainLock status (None if dashd not connected)
     #[serde(skip_serializing_if = "Option::is_none")]
     pub chainlock: Option<bool>,
+}
+
+#[derive(Serialize)]
+#[serde(untagged)]
+pub enum TxVin {
+    Coinbase {
+        coinbase: String,
+        sequence: u32,
+        n: u32,
+    },
+    Regular {
+        txid: String,
+        vout: u32,
+        sequence: u32,
+        n: u32,
+        #[serde(rename = "scriptSig")]
+        script_sig: ScriptSigResponse,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        addr: Option<String>,
+        #[serde(rename = "valueSat", skip_serializing_if = "Option::is_none")]
+        value_sat: Option<i64>,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        value: Option<f64>,
+        #[serde(rename = "doubleSpentTxID")]
+        double_spent_tx_id: Option<()>,
+    },
+}
+
+#[derive(Serialize)]
+pub struct ScriptSigResponse {
+    pub hex: String,
+}
+
+#[derive(Serialize)]
+pub struct TxVout {
+    pub value: String,
+    pub n: u32,
+    #[serde(rename = "scriptPubKey")]
+    pub script_pub_key: ScriptPubKeyResponse,
+    #[serde(rename = "spentTxId")]
+    pub spent_tx_id: Option<String>,
+    #[serde(rename = "spentIndex")]
+    pub spent_index: Option<u32>,
+    #[serde(rename = "spentHeight")]
+    pub spent_height: Option<u32>,
+}
+
+#[derive(Serialize)]
+pub struct ScriptPubKeyResponse {
+    pub hex: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub addresses: Option<Vec<String>>,
+    #[serde(rename = "type", skip_serializing_if = "Option::is_none")]
+    pub script_type: Option<String>,
 }
 
 #[derive(Serialize)]
@@ -305,17 +365,10 @@ pub async fn get_tx(
     txid.copy_from_slice(&txid_bytes);
     let internal_txid = reverse_hash(&txid);
 
-    let tx = state
+    let tx_record = state
         .db
         .get_tx(&internal_txid)
-        .map_err(|_| {
-            (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                Json(ErrorResponse {
-                    error: "Database error".to_string(),
-                }),
-            )
-        })?
+        .map_err(|_| db_error())?
         .ok_or_else(|| {
             (
                 StatusCode::NOT_FOUND,
@@ -325,22 +378,207 @@ pub async fn get_tx(
             )
         })?;
 
+    // Get raw transaction bytes and deserialize
+    let raw_bytes = state
+        .db
+        .get_raw_tx(&internal_txid)
+        .map_err(|_| db_error())?
+        .ok_or_else(|| {
+            (
+                StatusCode::NOT_FOUND,
+                Json(ErrorResponse {
+                    error: format!("Raw tx data not found: {}", txid_hex),
+                }),
+            )
+        })?;
+
+    let tx_size = raw_bytes.len();
+
+    let tx = Transaction::deserialize(&raw_bytes).map_err(|_| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(ErrorResponse {
+                error: "Failed to deserialize transaction".to_string(),
+            }),
+        )
+    })?;
+
+    // Get block info for blockhash and blocktime
+    let block_record = state
+        .db
+        .get_block_by_height(tx_record.block_height)
+        .map_err(|_| db_error())?;
+
+    let blockhash = block_record
+        .as_ref()
+        .map(|b| hash_to_display(&b.hash))
+        .unwrap_or_default();
+
+    let blocktime = block_record.as_ref().map(|b| b.time).unwrap_or(0);
+
+    // Compute confirmations
+    let tip_height = state
+        .db
+        .get_meta()
+        .ok()
+        .flatten()
+        .map(|m| m.tip_height)
+        .unwrap_or(tx_record.block_height);
+    let confirmations = tip_height - tx_record.block_height + 1;
+
+    let is_coinbase = tx.is_coinbase();
+
+    // Build vin array
+    let mut vin = Vec::with_capacity(tx.inputs.len());
+    let mut value_in_total: i64 = 0;
+
+    for (i, input) in tx.inputs.iter().enumerate() {
+        if is_coinbase {
+            vin.push(TxVin::Coinbase {
+                coinbase: hex::encode(&input.script_sig),
+                sequence: input.sequence,
+                n: i as u32,
+            });
+        } else {
+            // Look up the spent output's value and address via the UTXO
+            // or spent_by DB. We use get_raw_tx on the previous tx to
+            // get the output's scriptPubKey and value.
+            let prev_txid = input.previous_output.hash;
+            let prev_vout = input.previous_output.n;
+
+            let (addr, value_sat) = resolve_input_details(&state.db, &prev_txid, prev_vout);
+
+            if let Some(v) = value_sat {
+                value_in_total += v;
+            }
+
+            vin.push(TxVin::Regular {
+                txid: hash_to_display(&prev_txid),
+                vout: prev_vout,
+                sequence: input.sequence,
+                n: i as u32,
+                script_sig: ScriptSigResponse {
+                    hex: hex::encode(&input.script_sig),
+                },
+                addr,
+                value_sat,
+                value: value_sat.map(|v| v as f64 / 100_000_000.0),
+                double_spent_tx_id: None,
+            });
+        }
+    }
+
+    // Build vout array
+    let mut vout = Vec::with_capacity(tx.outputs.len());
+
+    for (i, output) in tx.outputs.iter().enumerate() {
+        let info = analyze_script(&output.script_pubkey);
+
+        let (addresses, script_type) = match (&info.script_type, &info.address_hash) {
+            (ScriptType::P2pkh, Some(hash)) => {
+                let addr = encode_address(hash, &info.script_type, false);
+                (addr.map(|a| vec![a]), Some("pubkeyhash".to_string()))
+            }
+            (ScriptType::P2sh, Some(hash)) => {
+                let addr = encode_address(hash, &info.script_type, false);
+                (addr.map(|a| vec![a]), Some("scripthash".to_string()))
+            }
+            (ScriptType::P2pk, Some(hash)) => {
+                let addr = encode_address(hash, &info.script_type, false);
+                (addr.map(|a| vec![a]), Some("pubkey".to_string()))
+            }
+            (ScriptType::OpReturn, _) => (None, Some("nulldata".to_string())),
+            _ => (None, None),
+        };
+
+        // Look up spent-by info
+        let (spent_tx_id, spent_index, spent_height) =
+            match state.db.get_spent_by(&internal_txid, i as u32) {
+                Ok(Some((stxid, sidx, sheight))) => {
+                    (Some(hash_to_display(&stxid)), Some(sidx), Some(sheight))
+                }
+                _ => (None, None, None),
+            };
+
+        vout.push(TxVout {
+            value: format!("{:.8}", output.value as f64 / 100_000_000.0),
+            n: i as u32,
+            script_pub_key: ScriptPubKeyResponse {
+                hex: hex::encode(&output.script_pubkey),
+                addresses,
+                script_type,
+            },
+            spent_tx_id,
+            spent_index,
+            spent_height,
+        });
+    }
+
+    let value_out = tx_record.value_out as f64 / 100_000_000.0;
+
+    let (value_in, fees) = if is_coinbase {
+        (None, None)
+    } else {
+        let vi = value_in_total as f64 / 100_000_000.0;
+        (Some(vi), Some(vi - value_out))
+    };
+
     // Query dashd for InstantSend/ChainLock status if available
     let (txlock, chainlock) = query_tx_locks(&state.rpc, &txid_hex).await;
 
     Ok(Json(TxResponse {
-        txid: hash_to_display(&tx.txid),
-        block_height: tx.block_height,
-        tx_index: tx.tx_index,
-        version: tx.version,
-        tx_type: tx.tx_type,
-        locktime: tx.lock_time,
-        value_out: tx.value_out as f64 / 100_000_000.0,
-        input_count: tx.input_count,
-        output_count: tx.output_count,
+        txid: hash_to_display(&tx_record.txid),
+        version: tx_record.version,
+        tx_type: if tx_record.tx_type > 0 {
+            Some(tx_record.tx_type)
+        } else {
+            None
+        },
+        locktime: tx_record.lock_time,
+        vin,
+        vout,
+        blockhash,
+        block_height: tx_record.block_height as i32,
+        confirmations,
+        time: blocktime,
+        blocktime,
+        is_coinbase: if is_coinbase { Some(true) } else { None },
+        value_out,
+        size: tx_size,
+        value_in,
+        fees,
         txlock,
         chainlock,
     }))
+}
+
+/// Resolve input address and value by looking up the previous transaction's output.
+fn resolve_input_details(
+    db: &DainoDB,
+    prev_txid: &[u8; 32],
+    prev_vout: u32,
+) -> (Option<String>, Option<i64>) {
+    // First try: look up the previous tx's raw bytes and extract the output
+    if let Ok(Some(raw)) = db.get_raw_tx(prev_txid)
+        && let Ok(prev_tx) = Transaction::deserialize(&raw)
+        && let Some(output) = prev_tx.outputs.get(prev_vout as usize)
+    {
+        let info = analyze_script(&output.script_pubkey);
+        let addr = info
+            .address_hash
+            .and_then(|h| encode_address(&h, &info.script_type, false));
+        return (addr, Some(output.value));
+    }
+
+    // Fallback: check if there's still a live UTXO (shouldn't be, since it's spent)
+    if let Ok(Some(utxo)) = db.get_utxo(prev_txid, prev_vout) {
+        let addr = utxo
+            .addr_hash
+            .and_then(|h| encode_address(&h, &ScriptType::P2pkh, false));
+        return (addr, Some(utxo.value));
+    }
+
+    (None, None)
 }
 
 // -- Address response types --
